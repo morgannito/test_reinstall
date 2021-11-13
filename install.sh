02e8b0b57f56c1d446694158ad7568b1feb51feb
morgannito <mriu.morgannito@gmail.com>
2021-11-13 18:38:44 +0100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,11 @@
+sudo apt-get update && sudo apt-get upgrade
+sudo apt get install git
+sudo apt get install npm
+sudo apt get install nodejs
+sudo apt get install apache2
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update && sudo apt-get upgradesudo apt install php8.0 libapache2-mod-php8.0
+sudo systemctl restart apache2
+sudo apt-get update && sudo apt-get upgrade
+sudo apt install php8.0-mysql php8.0-gd
