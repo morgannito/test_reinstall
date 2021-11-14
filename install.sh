cf171f3de034d3666d8053967622e2b811963e71
toto <mriu.morgannito@gmail.com>
2021-11-15 00:21:57 +0100

    amélioration

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@ sudo apt-get update && sudo apt-get upgrade
 sudo apt install snapd
 sudo snap install pycharm-community --classic
 sudo apt-get update && sudo apt-get upgrade
-sudo apt get install git
-sudo apt get install npm
-sudo apt get install nodejs
-sudo apt get install apache2
+sudo apt-get install git
+sudo apt-get install npm
+sudo apt-get install nodejs
+sudo apt-get install apache2
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update && sudo apt-get upgradesudo apt install php8.0 libapache2-mod-php8.0
